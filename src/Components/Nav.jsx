9d1b6656fedd2760ca56c2d8552f148fa85ebea8
pinargultekin <pinargultekin@yahoo.com>
2020-05-29 15:01:52 -0400

    navbar icon added

--- src/Components/Nav.jsx
+++ src/Components/Nav.jsx
@@ -1,14 +1,17 @@
 import React from "react";
 import {Navbar, Brand, Link, Nav} from "react-bootstrap";
+import {FiShoppingCart} from 'react-icons/fi';
+import {BsFillPersonFill} from 'react-icons/bs';
+import {MdRestaurantMenu} from 'react-icons/md';
 import Logo from "./Logo";
 function Navigation() {
     return <Navbar className="navBar">
     <Navbar.Brand href="#home"><Logo /> </Navbar.Brand>
     <Navbar.Toggle />
     <Navbar.Collapse className="justify-content-end">
-    <Nav.Link className="navLink" href="#menu">Menu</Nav.Link>
-      <Nav.Link className="navLink" href="#cart">Cart</Nav.Link>
-      <Nav.Link className="navLink" href="#login">Log In</Nav.Link>
+    <Nav.Link className="navLink" href="#menu"><MdRestaurantMenu/></Nav.Link>
+<Nav.Link className="navLink" href="#cart"><FiShoppingCart/></Nav.Link>
+      <Nav.Link className="navLink" href="#login"><BsFillPersonFill/></Nav.Link>
     </Navbar.Collapse>
   </Navbar>
 }
