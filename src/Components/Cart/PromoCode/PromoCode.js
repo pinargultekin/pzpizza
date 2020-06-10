342f1252ad76a90e3a3a17e11764cd2f3edb198f
pinargultekin <pinargultekin@yahoo.com>
2020-06-10 13:16:21 -0400

    Cart files recovered

---
+++ src/Components/Cart/PromoCode/PromoCode.js
@@ -0,0 +1,85 @@
+import React, { Component } from 'react';
+import {
+  Button,
+  Collapse,
+  Well,
+  Form,
+  Row,
+  Col,
+  FormGroup,
+  FormLabel,
+  FormControl
+} from 'react-bootstrap';
+
+import { connect } from 'react-redux';
+import { handleChange } from '../../actions/promoCodeActions';
+
+class PromoCodeDiscount extends Component {
+  // we no longer need a constructor as the state will be managed in redux
+
+  constructor(props, context) {
+    super(props, context);
+
+    this.state = {
+      open: false
+    };
+  }
+
+  handleChange = e => {
+    // the setState below will be sent to redux
+    // this.setState({ value: e.target.value });
+    this.props.handleChange(e);
+  };
+
+  render() {
+    return (
+      <div>
+        <Button
+          className="promo-code-button"
+          bsStyle="link"
+          onClick={() => this.setState({ open: !this.state.open })}
+        >
+          {this.state.open === false ? `Apply ` : `Hide `}
+          promo code {this.state.open === false ? `+` : `-`}
+        </Button>
+        <Collapse in={this.state.open}>
+          <div>
+            <Well>
+              <Row className="show-grid">
+                <Col md={12}>
+                  <Form>
+                    <FormGroup controlId="formInlineName">
+                      <FormLabel>Promo Code</FormLabel>{' '}
+                      <FormControl
+                        type="text"
+                        placeholder="Enter promo code"
+                        value={this.props.promoCode}
+                        onChange={this.handleChange}
+                      />
+                    </FormGroup>{' '}
+                    <Button
+                      block
+                      bsStyle="success"
+                      className="btn-round"
+                      // type="submit"
+                      disabled={this.props.isDisabled}
+                      onClick={this.props.giveDiscount}
+                    >
+                      Apply
+                    </Button>
+                  </Form>
+                </Col>
+              </Row>
+            </Well>
+          </div>
+        </Collapse>
+      </div>
+    );
+  }
+}
+
+const mapStateToProps = state => ({
+  promoCode: state.promoCode.value
+});
+
+export default connect(mapStateToProps, { handleChange })(PromoCodeDiscount);
---
+++ src/Components/Cart/TaxAndFees/TaxAndFees.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import { Row, Col } from 'react-bootstrap';
+
+export default class TaxesFees extends Component {
+  render() {
+    return (
+      <Row className="show-grid">
+        <Col md={6}>Est. taxes & fees </Col>
+        <Col md={6}>
+          <strong>{`$${this.props.taxes}`}</strong>
+        </Col>
+      </Row>
+    );
+  }
+}
