886b42bbac1557c09e2c16d30a21155c0114c33d
pinargultekin <pinargultekin@yahoo.com>
2020-06-10 11:47:24 -0400

    reset

--- src/Components/Navbar/Logo.jsx
+++
@@ -1,6 +0,0 @@
-import React from "react";
-
-function Logo(){
-    return <img className="logo" alt = "Logo" src="../../images/PZNewLogo.png"/>
-}
-export default Logo;
--- src/Components/Promophotos/HeaderPhoto.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-
-function HeaderPhoto(){
-    return <div className="headerp text-center">
-<img className="headerPic" src="../../images/header.png" alt="HeaderPic"/>
-    </div>
-}
-export default HeaderPhoto;
