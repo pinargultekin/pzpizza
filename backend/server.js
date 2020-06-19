f890523597248ae7e15e11b48941750dff9a95b7
ZAFER OZCAN <zaferozzcan@gmail.com>
2020-06-19 15:47:56 -0400

    Add debugged version

--- backend/server.js
+++ backend/server.js
@@ -11,11 +11,14 @@ app.use(cors());
 app.use(express.json());
 
 const uri = process.env.ATLAS_URI;
-mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true }
-);
+mongoose.connect(uri, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true
+});
 const connection = mongoose.connection;
 connection.once('open', () => {
-  console.log("MongoDB database connection established successfully");
+    console.log("MongoDB database connection established successfully");
 })
 
 
