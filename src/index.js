c8ed19f95d21a3d345569ec178bb98b3907d2267
pinargultekin <pinargultekin@yahoo.com>
2020-06-20 09:07:26 -0400

    provider added

--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from "../src/Components/App/App.jsx";
-// import { Provider } from 'react-redux';
-// import store from './store';
+import { Provider } from 'react-redux';
+import store from './store';
 
 
 ReactDOM.render(
-    <App />,document.getElementById('root')
+  <Provider store={store}>
+      <App />
+    </Provider>,
+    document.getElementById('root')
   );
