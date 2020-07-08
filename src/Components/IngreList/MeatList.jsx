a02f2080e8416ef2b673fc1ee9b6960ba57afefd
pinargultekin <pinargultekin@yahoo.com>
2020-07-08 14:43:24 -0400

    for loop used for form group & checked existed ingredients

--- src/Components/IngreList/MeatList.jsx
+++ src/Components/IngreList/MeatList.jsx
@@ -1,56 +1,48 @@
 import React from "react";
 import { Form } from "react-bootstrap";
-import menu from "../../Menudata/menu";
+import Menu from "../../Menudata/menu";
 
 function MeatList(props) {
 
   const header = props.header;
-  const [{ itemMeatTop: pizzaMeatTop },
-    { itemMeatTop: pastaMeatTop },
-    { itemMeatTop: saladMeatTop }] = menu;
-  const [pzmt1, pzmt2, pzmt3, pzmt4, pzmt5, pzmt6, pzmt7, pzmt8] = pizzaMeatTop;
-  const [psmt1, psmt2, psmt3, psmt4, psmt5, psmt6, psmt7, psmt8] = pastaMeatTop;
-  const [sdmt1, sdmt2, sdmt3] = saladMeatTop;
+//   const [{ itemMeatTop: pizzaMeatTop },
+//     { itemMeatTop: pastaMeatTop },
+//     { itemMeatTop: saladMeatTop }] = menu;
+//   const [pzmt1, pzmt2, pzmt3, pzmt4, pzmt5, pzmt6, pzmt7, pzmt8] = pizzaMeatTop;
+//   const [psmt1, psmt2, psmt3, psmt4, psmt5, psmt6, psmt7, psmt8] = pastaMeatTop;
+//   const [sdmt1, sdmt2, sdmt3] = saladMeatTop;
 
-
+  const pizzaMeatTop = Menu[0].itemMeatTop;
+  const pastaMeatTop = Menu[1].itemMeatTop;
+  const saladMeatTop = Menu[2].itemMeatTop;
   const itemIngre = props.ingredients;
-  const intersecPz = itemIngre.filter(element => pizzaMeatTop.includes(element))
-  const intersecPs = itemIngre.filter(element => pastaMeatTop.includes(element))
-  const intersecSd = itemIngre.filter(element => saladMeatTop.includes(element))
+  const intersecPz1 = itemIngre.filter(element => pizzaMeatTop.includes(element))
+  const intersecPs1 = itemIngre.filter(element => pastaMeatTop.includes(element))
+  const intersecSd1 = itemIngre.filter(element => saladMeatTop.includes(element))
 
   return (
     <Form className="ckList">
 
       {
-        //! LATER UPDATE: use for loop
         header === "Pizza" ?
-          <Form.Group >
-            <Form.Check className="mtcheckbox" value={pzmt1} type="checkbox" label={pzmt1} />
-            <Form.Check className="mtcheckbox" value={pzmt2} type="checkbox" label={pzmt2} />
-            <Form.Check className="mtcheckbox" value={pzmt3} type="checkbox" label={pzmt3} />
-            <Form.Check className="mtcheckbox" value={pzmt4} type="checkbox" label={pzmt4} />
-            <Form.Check className="mtcheckbox" value={pzmt5} type="checkbox" label={pzmt5} />
-            <Form.Check className="mtcheckbox" value={pzmt6} type="checkbox" label={pzmt6} />
-            <Form.Check className="mtcheckbox" value={pzmt7} type="checkbox" label={pzmt7} />
-            <Form.Check className="mtcheckbox" value={pzmt8} type="checkbox" label={pzmt8} />
-          </Form.Group>
-          : header === "Pasta" ?
-            <Form.Group >
-              <Form.Check className="mtcheckbox" value={psmt1} type="checkbox" label={psmt1} />
-              <Form.Check className="mtcheckbox" value={psmt2} type="checkbox" label={psmt2} />
-              <Form.Check className="mtcheckbox" value={psmt3} type="checkbox" label={psmt3} />
-              <Form.Check className="mtcheckbox" value={psmt4} type="checkbox" label={psmt4} />
-              <Form.Check className="mtcheckbox" value={psmt5} type="checkbox" label={psmt5} />
-              <Form.Check className="mtcheckbox" value={psmt6} type="checkbox" label={psmt6} />
-              <Form.Check className="mtcheckbox" value={psmt7} type="checkbox" label={psmt7} />
-              <Form.Check className="mtcheckbox" value={psmt8} type="checkbox" label={psmt8} />
-            </Form.Group>
-            :
-            <Form.Group >
-              <Form.Check className="mtcheckbox" value={sdmt1} type="checkbox" label={sdmt1} />
-              <Form.Check className="mtcheckbox" value={sdmt2} type="checkbox" label={sdmt2} />
-              <Form.Check className="mtcheckbox" value={sdmt3} type="checkbox" label={sdmt3} />
-            </Form.Group>
+        <Form.Group>
+                {pizzaMeatTop.map(item => {
+                return (<Form.Check checked={intersecPz1.includes(item)} className="mtcheckbox" value={item} type="checkbox" label={item} />)
+                })}
+        </Form.Group>
+        : 
+        header === "Pasta" ?
+        <Form.Group >
+                {pastaMeatTop.map(item => {
+                return (<Form.Check checked={intersecPs1.includes(item)} className="mtcheckbox" value={item} type="checkbox" label={item} />)
+                })}
+        </Form.Group>
+        :
+        <Form.Group>
+                {saladMeatTop.map(item => {
+                return (<Form.Check checked={intersecSd1.includes(item)} className="mtcheckbox" value={item} type="checkbox" label={item} />)
+                })}
+        </Form.Group>
       }
     </Form>
   )
--- src/Components/IngreList/SauceList.jsx
+++ src/Components/IngreList/SauceList.jsx
@@ -1,57 +1,54 @@
 import React from "react";
 import { Form } from "react-bootstrap";
-import menu from "../../Menudata/menu";
+import Menu from "../../Menudata/menu";
 
 function SauceList(props) {
-  const header = props.header;
-  const [{ itemSauce: pizzaSauce },
-    { itemSauce: pastaSauce },
-    { itemSauce: saladSauce }] = menu;
-  const [pzs1, pzs2, pzs3, pzs4, pzs5, pzs6, pzs7] = pizzaSauce;
-  const [pss1, pss2, pss3, pss4, pss5,] = pastaSauce;
-  const [sds1, sds2, sds3, sds4, sds5, sds6, sds7, sds8, sds9, sds10] = saladSauce;
+  // const header = props.header;
+  // const [{ itemSauce: pizzaSauce },
+  //   { itemSauce: pastaSauce },
+  //   { itemSauce: saladSauce }] = menu;
+  // const [pzs1, pzs2, pzs3, pzs4, pzs5, pzs6, pzs7] = pizzaSauce;
+  // const [pss1, pss2, pss3, pss4, pss5,] = pastaSauce;
+  // const [sds1, sds2, sds3, sds4, sds5, sds6, sds7, sds8, sds9, sds10] = saladSauce;
+
+  // const itemIngre = props.ingredients;
+  // const intersecPz = itemIngre.filter(element => pizzaSauce.includes(element))
+  // const intersecPs = itemIngre.filter(element => pastaSauce.includes(element))
+  // const intersecSd = itemIngre.filter(element => saladSauce.includes(element))
+
+  const pizzaSauce = Menu[0].itemSauce;
+  const pastaSauce = Menu[1].itemSauce;
+  const saladSauce = Menu[2].itemSauce;
 
   const itemIngre = props.ingredients;
-  const intersecPz = itemIngre.filter(element => pizzaSauce.includes(element))
-  const intersecPs = itemIngre.filter(element => pastaSauce.includes(element))
-  const intersecSd = itemIngre.filter(element => saladSauce.includes(element))
-
-
+  // console.log(itemIngre);
+  const intersecPz2 = itemIngre.filter(element => pizzaSauce.includes(element))
+  const intersecPs2 = itemIngre.filter(element => pastaSauce.includes(element))
+  const intersecSd2 = itemIngre.filter(element => saladSauce.includes(element))
+  // const allIntersec = [intersecPz, intersecPs, intersecSd]
+  const header = props.header;
+  // console.log(intersecPz)
   return (<Form className="ckList">
     {
-      //! LATER UPDATE: use for loop
-      header === "Pizza" ?
-        <Form.Group >
-          <Form.Check className="mtcheckbox" value={pzs1} type="checkbox" label={pzs1} />
-          <Form.Check className="mtcheckbox" value={pzs2} type="checkbox" label={pzs2} />
-          <Form.Check className="mtcheckbox" value={pzs3} type="checkbox" label={pzs3} />
-          <Form.Check className="mtcheckbox" value={pzs4} type="checkbox" label={pzs4} />
-          <Form.Check className="mtcheckbox" value={pzs5} type="checkbox" label={pzs5} />
-          <Form.Check className="mtcheckbox" value={pzs6} type="checkbox" label={pzs6} />
-          <Form.Check className="mtcheckbox" value={pzs7} type="checkbox" label={pzs7} />
-        </Form.Group>
-        :
-        header === "Pasta" ?
-          <Form.Group >
-            <Form.Check className="mtcheckbox" value={pss1} type="checkbox" label={pss1} />
-            <Form.Check className="mtcheckbox" value={pss2} type="checkbox" label={pss2} />
-            <Form.Check className="mtcheckbox" value={pss3} type="checkbox" label={pss3} />
-            <Form.Check className="mtcheckbox" value={pss4} type="checkbox" label={pss4} />
-            <Form.Check className="mtcheckbox" value={pss5} type="checkbox" label={pss5} />
-          </Form.Group>
-          :
-          <Form.Group >
-            <Form.Check className="mtcheckbox" value={sds1} type="checkbox" label={sds1} />
-            <Form.Check className="mtcheckbox" value={sds2} type="checkbox" label={sds2} />
-            <Form.Check className="mtcheckbox" value={sds3} type="checkbox" label={sds3} />
-            <Form.Check className="mtcheckbox" value={sds4} type="checkbox" label={sds4} />
-            <Form.Check className="mtcheckbox" value={sds5} type="checkbox" label={sds5} />
-            <Form.Check className="mtcheckbox" value={sds6} type="checkbox" label={sds6} />
-            <Form.Check className="mtcheckbox" value={sds7} type="checkbox" label={sds7} />
-            <Form.Check className="mtcheckbox" value={sds8} type="checkbox" label={sds8} />
-            <Form.Check className="mtcheckbox" value={sds9} type="checkbox" label={sds9} />
-            <Form.Check className="mtcheckbox" value={sds10} type="checkbox" label={sds10} />
-          </Form.Group>
+    header === "Pizza" ?
+    <Form.Group>
+      {pizzaSauce.map(item => {
+      return (<Form.Check checked={intersecPz2.includes(item)} className="mtcheckbox" value={item} type="checkbox" label={item} />)
+      })}
+    </Form.Group>
+    :
+    header === "Pasta" ?
+    <Form.Group >
+        {pastaSauce.map(item => {
+        return (<Form.Check checked={intersecPs2.includes(item)} className="mtcheckbox" value={item} type="checkbox" label={item} />)
+        })}
+    </Form.Group>
+    :
+    <Form.Group>
+      {saladSauce.map(item => {
+        return (<Form.Check checked={intersecSd2.includes(item)} className="mtcheckbox" value={item} type="checkbox" label={item} />)
+      })}
+    </Form.Group>
     }
 
   </Form>
