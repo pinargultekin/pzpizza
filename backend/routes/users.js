dea1f1f26989b07a90be48d0b02edd7446946e07
ZAFER OZCAN <zaferozzcan@gmail.com>
2020-07-17 14:04:13 -0400

    Add user login

---
+++ backend/routes/users.js
@@ -0,0 +1,122 @@
+require('dotenv').config();
+const express = require('express')
+const users = express.Router()
+const cors = require('cors')
+const jwt = require('jsonwebtoken')
+const bcrypt = require('bcrypt')
+
+
+
+const User = require('../models/user.model')
+users.use(cors())
+
+process.env.SECRET_KEY = 'secret'
+
+users.post('/add', (req, res) => {
+
+    const firstName = req.body.firstName;
+    const lastName = req.body.lastName;
+    const email = req.body.email;
+    const phoneNumber = req.body.phoneNumber;
+    const password = req.body.password;
+    const address = req.body.address;
+    const city = req.body.city;
+    const state = req.body.state;
+    const zip = req.body.zip;
+
+    const newUser = new User({
+        firstName,
+        lastName,
+        email,
+        phoneNumber,
+        password,
+        address,
+        city,
+        state,
+        zip
+    });
+
+    User.findOne({
+        email: req.body.email
+    })
+        .then(user => {
+            if (!user) {
+                bcrypt.hash(req.body.password, 10, (err, hash) => {
+                    newUser.password = hash
+                    User.create(newUser)
+                        .then(user => {
+                            res.json({ status: user.email + 'Registered!' })
+                        })
+                        .catch(err => {
+                            res.send('error: ' + err)
+                        })
+                })
+            } else {
+                res.json({ error: 'User already exists' })
+            }
+        })
+        .catch(err => {
+            res.send('error: ' + err)
+        })
+})
+
+users.post('/login', (req, res) => {
+    User.findOne({
+        email: req.body.email
+
+    })
+        .then(user => {
+            if (user) {
+                if (bcrypt.compare(req.body.password, user.password)) {
+                    // Passwords match
+                    const payload = {
+                        _id: user._id,
+                        firstName: user.firstName,
+                        lastName: user.lastName,
+                        email: user.email
+                    }
+                    let token = jwt.sign(payload, process.env.SECRET_KEY, {
+                        expiresIn: 1440
+                    })
+                    console.log(token);
+                    res.send(token)
+                } else {
+                    // Passwords don't match
+                    res.json({ error: 'Password did not match' })
+                }
+            } else {
+                res.json({ error: 'User does not exist' })
+            }
+        })
+        .catch(err => {
+            res.send('error: ' + err)
+        })
+})
+
+users.get('/account', (req, res) => {
+    var decoded = jwt.verify(req.headers['authorization'], process.env.SECRET_KEY)
+
+    User.findOne({
+        _id: decoded._id
+
+    })
+        .then(user => {
+            if (user) {
+                res.json(user)
+            } else {
+                res.send('User does not exist')
+            }
+            console.log(user)
+        })
+        .catch(err => {
+            res.send('error: ' + err)
+        })
+})
+
+module.exports = users
+
+
+
+
+
+
--- src/Components/Form/Login.jsx
+++ src/Components/Form/Login.jsx
@@ -1,13 +1,67 @@
 import React, { Component } from "react";
-import {Form, Col, Button} from "react-bootstrap";
+import { Form, Col, Button } from "react-bootstrap";
+// import { login } from "./UserFunctions"
+import axios from "axios"
+import { withRouter } from 'react-router'
+
+
+
+
+
+
+// class
 class Login extends Component {
-    render(){
-        
-        return <div className="Login-cont">
-            <h2 className="registerh2"><a href="/register">Register</a> <span style={{float:"right"}}><a href="/login">Log In</a></span></h2>
-            <hr/>
-            <br/>
-            <Form>
+
+
+  constructor() {
+    super()
+    this.state = {
+      email: '',
+      password: '',
+
+    }
+
+    this.onChange = this.onChange.bind(this)
+    this.onSubmit = this.onSubmit.bind(this)
+  }
+  onChange(e) {
+    this.setState({ [e.target.name]: e.target.value })
+  }
+
+  onSubmit(e) {
+    e.preventDefault()
+
+    var user = {
+      email: this.state.email,
+      password: this.state.password
+    }
+    console.log(user);
+
+    axios
+      .post('http://localhost:5000/users/login', {
+        email: user.email,
+        password: user.password
+      })
+      .then(response => {
+        localStorage.setItem('usertoken', response.data)
+        console.log(response.data);
+        this.props.history.push(`/account`)
+        return response.data
+      })
+      .catch(err => {
+        console.log(err)
+      })
+
+  }
+
+
+  render() {
+
+    return <div className="Login-cont">
+      <h2 className="registerh2"><a href="/register">Register</a> <span style={{ float: "right" }}><a href="/login">Log In</a></span></h2>
+      <hr />
+      <br />
+      <Form onSubmit={this.onSubmit}>
         <Form.Row className="log-form">
           <Form.Group as={Col} md="12" controlId="validationCustom01">
             <Form.Label>Email</Form.Label>
@@ -15,24 +69,30 @@ class Login extends Component {
               required
               type="mail"
               placeholder="email@mail.com"
+              name="email"
+              onChange={
+                this.onChange}
             />
             <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
           </Form.Group>
-          </Form.Row>
-          <Form.Row className="log-form">
+        </Form.Row>
+        <Form.Row className="log-form">
           <Form.Group as={Col} md="12" controlId="validationCustom02">
             <Form.Label>Password</Form.Label>
             <Form.Control
               required
               type="text"
               placeholder="Password"
+              name="password"
+              onChange={
+                this.onChange}
             />
             <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
           </Form.Group>
         </Form.Row>
-        <Button href="/account" className= "login-btn btn-outline-danger" type="submit">Sign In</Button>
+        <Button className="login-btn btn-outline-danger" type="submit">Sign In</Button>
       </Form>
-      </div>
-    };
+    </div>
+  };
 }
-export default Login;
+export default withRouter(Login);
